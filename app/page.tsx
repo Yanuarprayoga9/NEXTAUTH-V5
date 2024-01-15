630cb9e459e08c8223d006a50f09930a60b1fbb5
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-01-15 22:39:09 +0700

    login button

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,22 @@
 import { Button } from "@/components/ui/button";
 import { Poppins } from "next/font/google";
-import { cn } from "@/lib/utils"
+import { cn } from "@/lib/utils";
+import { LoginButton } from "@/components/auth/login-button";
 
 const font = Poppins({
-  subsets:['latin'],
-  weight:['600']
-})
+  subsets: ["latin"],
+  weight: ["600"],
+});
 export default function Home() {
   return (
-    <main className="flex h-full flex-col items-center justify-center ">
-      <div className="space-y-6 bg-slate-100 p-20 rounded-lg">
-        <h1 className={cn("text-6xl font-semibold",font.className)}>Auth</h1>
+    <main className="flex h-full flex-col items-center justify-center  bg-slate-100  ">
+      <div className="space-y-6 py-56 px-32 rounded-lg text-center bg-white">
+        <h1 className={cn("text-6xl font-semibold", font.className)}>Auth</h1>
         <p>a simple authentication apps using next auth</p>
         <div className="">
-          <Button size={"lg"}>Sign In</Button>
+          <LoginButton >
+            <Button size={"lg"}>Sign In</Button>
+          </LoginButton>
         </div>
       </div>
     </main>
