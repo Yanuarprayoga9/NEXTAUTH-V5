172be70e60634da8677ac547c91b19599fe9238e
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-01-16 09:16:39 +0700

    back button

--- components/auth/card-wrapper.tsx
+++ components/auth/card-wrapper.tsx
@@ -8,7 +8,7 @@ import {
 } from "@/components/ui/card";
 import { Header } from "@/components/auth/header";
 import { Social } from "./social";
-
+import { BackButton } from "./back-button";
 interface CardWrapperProps {
   children: React.ReactNode;
   headerLabel: string;
@@ -35,6 +35,9 @@ export const CardWrapper = ({
           <Social />
         </CardFooter>
       )}
+      <CardFooter>
+        <BackButton href={backButtonHref} label={backButtonLabel} />
+      </CardFooter>
     </Card>
   );
 };
