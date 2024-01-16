b374a9b59c02c0ae33e05fa5e9c047f8fd259de6
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-01-16 08:36:40 +0700

    social login page

--- components/auth/social.tsx
+++ components/auth/social.tsx
@@ -1,7 +1,20 @@
-"use client"
-
+"use client";
+import { Button } from "@/components/ui/button";
+import { FcGoogle } from "react-icons/fc";
+import { FaGithub } from "react-icons/fa";
 export const Social = () => {
-return (
-    <div className="">Social</div>
-)
-}
+  return (
+    <div className="flex items-center justify-center w-full gap-x-2">
+      <Button variant={"outline"} 
+        className="w-full"
+        >
+        <FcGoogle className="w-5 h-5" />
+      </Button>
+      <Button variant={"outline"}
+      className="w-full"
+      >
+        <FaGithub className="w-5 h-5" />
+      </Button>
+    </div>
+  );
+};
