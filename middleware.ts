7ea22b5c6e0ce28cd25398f75166a743d7c920e9
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-01-17 11:15:38 +0700

    define routes

--- middleware.ts
+++ middleware.ts
@@ -1,10 +1,44 @@
-import { auth } from "./auth"
+import authConfig from "./auth.config";
+import NextAuth from "next-auth";
+import {
+  authRoutes,
+  apiAuthPrefix,
+  publicRoutes,
+  DEFAULT_ISLOGIN_REDIRECT,
+} from "@/routes";
+export const { auth } = NextAuth(authConfig);
 
 export default auth((req) => {
-  // req.auth
-})
+    const { nextUrl } = req;
+    // const isLoggedIn = !!req.auth;
+    const isLoggedIn = true;
+  
+    const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+    const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+    const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+  
+    if (isApiAuthRoute) {
+
+    }
+    if(isLoggedIn){
+        if(isAuthRoute){
+            return Response.redirect(new URL(DEFAULT_ISLOGIN_REDIRECT, nextUrl)) 
+        }
+    }else{
+        if(isApiAuthRoute){
+            return Response.redirect(new URL(DEFAULT_ISLOGIN_REDIRECT, nextUrl)) 
+        }
+    }
+    
+    if (isAuthRoute) {
+      if (!isLoggedIn) {
+        return Response.redirect(new URL(DEFAULT_ISLOGIN_REDIRECT, nextUrl))
+      }
+      return null;
+    }
+});
 
 // Optionally, don't invoke Middleware on some paths
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-}
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
