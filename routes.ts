ff0fafa3a1f6598fec98f02a3451bc91a0db8d59
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-01-17 11:16:02 +0700

    create routes programmaticly

---
+++ routes.ts
@@ -0,0 +1,31 @@
+/**
+ * An array  of routes that are accessible to the public
+ * Theseroutes do no require authentication
+ * @type {string[]}
+ */
+
+export const publicRoutes:string[] = ["/"];
+
+/**
+ * An array  of routes that are use fpr authentication 
+ * Theseroutes do no require authentication
+ * @type {string[]}
+ */
+
+export const authRoutes: string[] = ["/auth/login", "/auth/register"];
+
+
+/**
+ * An array  of routes that are use fpr authentication 
+ * Theseroutes that start with this prefix ares used for API authentication purposes 
+ * @type {string[]}
+ */
+export const apiAuthPrefix: string = "/api/auth";
+
+/*
+* The default redirect path after logging in | users is login
+*/
+
+export const DEFAULT_ISLOGIN_REDIRECT = "/settings"
+
+
