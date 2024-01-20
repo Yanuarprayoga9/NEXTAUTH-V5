46248cf188121780b6bc5c412e3060c9cd8755f5
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-01-20 07:20:41 +0700

    fix some error in param session({token})

--- auth.ts
+++ auth.ts
@@ -2,7 +2,7 @@ import NextAuth, { Session } from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import authConfig from "@/auth.config";
 import { db } from "@/lib/db";
-
+import { JWT } from "next-auth/jwt";
 import { getUserById } from "./data/user";
 export const {
   handlers: { GET, POST },
@@ -11,7 +11,8 @@ export const {
   signOut,
 } = NextAuth({
   callbacks: {
-    async session({ session, token }) {
+    async session(args: any) {
+      const { session, token } = args as { session: Session; token: JWT };
       const user = await db.user.findUnique({ where: { id: token.sub } });
       if (token.sub && session.user) {
         session.user.id = token.sub;
