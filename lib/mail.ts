a25ba369d9f649b1e1ce38057595a89fcd2575fe
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-01-21 10:36:54 +0700

    resend sendmailer

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,1 +1,15 @@
-console.log("test mail");
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export const sendEmailVerifictionToken = async (email: string, token: string) => {
+  const confirmLink = `${process.env.BASE_URL}/auth/new-verification?token=${token}`;
+  console.log({confirmLink,email})
+   await resend.emails.send({
+    from: "onboarding@resend.dev",
+    to: email,
+    subject: "Confirm your email",
+    html: `<p>Click <a href="${confirmLink}">here</a> to confirm email.</p>`,
+  });
+
+};
