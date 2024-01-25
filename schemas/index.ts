0e8c0b0c22262e8e90694284d8cb5f57a5590752
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-01-25 12:27:32 +0700

    create reset sechemas

--- schemas/index.ts
+++ schemas/index.ts
@@ -20,3 +20,8 @@ export const LoginSchema = z.object({
       message: "Name is required",
     }),
   });
+  export const ResetSchema = z.object({
+    email: z.string().email({
+      message: "Email is required",
+    }),
+  });
