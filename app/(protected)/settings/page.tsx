a3ce35f2d243541d7ecaee0c27f69d3f8790287b
Yanuarprayoga9 <yanuarprayogat@gmail.com>
2024-01-26 09:19:05 +0700

    logout functional

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -1,25 +1,19 @@
-import { auth, signOut } from "@/auth"
-import { currentRole } from "@/lib/auth"
+import { logout } from "@/actions/logout";
+import { auth, signOut } from "@/auth";
+import { currentRole } from "@/lib/auth";
 
 const page = async () => {
-    const session = await auth()
-    const role = await currentRole()
-    console.log(role)
+  const session = await auth();
+  const role = await currentRole();
+  console.log(role);
   return (
     <div>
       {JSON.stringify(session)}
-      <form action={
-        async ()=>{
-          "use server"
-          await signOut();
-        }
-      }>
-        <button type="submit">
-          Sign Out
-        </button>
+      <form action={logout}>
+        <button type="submit">Sign Out</button>
       </form>
     </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
